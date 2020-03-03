b6400d764b50eba545bdcb958fc2147784063acc
jun9130 <jun.nakama1030@gmail.com>
2020-03-03 16:29:10 +0900

    shopsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_02_080208) do
+ActiveRecord::Schema.define(version: 2020_03_03_065726) do
+
+  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "access"
+    t.string "address"
+    t.string "business_hours"
+    t.string "holiday"
+    t.integer "seat"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shops_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_02_02_080208) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shops", "users"
 end
