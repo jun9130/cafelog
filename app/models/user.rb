f096f0f200772ea121ebfc6a090f817594293b56
jun9130 <jun.nakama1030@gmail.com>
2020-03-03 16:30:30 +0900

    userモデル・shopモデル アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :shops
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name,  presence: true
