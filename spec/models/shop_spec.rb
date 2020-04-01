92e0283e476e42682a335b36a5d396970e02913d
jun9130 <jun.nakama1030@gmail.com>
2020-04-01 20:49:49 +0900

    Shop.rbのテスト

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+describe Shop do
+  describe '#create' do
+    it "name,access,address,user_idがあれば登録できる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "nameが空の時は保存できない" do
+    shop = build(:shop, name: "")
+    shop.valid?
+    expect(shop.errors[:name]).to include("を入力してください")
+    end
+
+    it "accessが空の時は保存できない" do
+      shop = build(:shop, access: "")
+      shop.valid?
+      expect(shop.errors[:access]).to include("を入力してください")
+      end
+
+    it "addressが空の時は保存できない" do
+      shop = build(:shop, address: "")
+      shop.valid?
+      expect(shop.errors[:address]).to include("を入力してください")
+      end
+
+    it "user_idが紐づいていないは保存できない" do
+      shop = build(:shop, user_id: "")
+      shop.valid?
+      expect(shop.errors[:user]).to include("を入力してください")
+      end
+  end
 end
