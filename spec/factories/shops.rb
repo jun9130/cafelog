32dadcecd41610329101837401457069041ad866
jun9130 <jun.nakama1030@gmail.com>
2020-04-02 22:52:28 +0900

    modify

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     access  {"渋谷駅"}
     address {"東京都渋谷区渋谷1-1-1"}
     user_id {"1"}
+    user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,50 +2,50 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
-    it "name,email,password,password_confirmationがあれば登録できる" do
+    it "名前,メールアドレス,パスワード,パスワード(確認)があれば登録できる" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    it "nameが空の時は登録できない" do
+    it "名前がブランクだと登録できない" do
       user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
-    it "emailが空の時は登録できない" do
+    it "メールアドレスがブランクだと登録できない" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
 
-    it "passwordが空の時は登録できない" do
+    it "パスワードがブランクだと登録できない" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
 
-    it "passwordが存在してもpassword_confirmationが空では登録できない" do
+    it "パスワードを入力してもパスワード(確認)がブランクだと登録ができない" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
-    it "重複したemailは登録できない" do
+    it "既に登録しているメールアドレスは登録できない" do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
-    it "passwordが6文字以上であれば登録できる " do
-      user = build(:user, password: "000000", password_confirmation: "000000")
+    it "パスワードが6文字以上であれば登録できる " do
+      user = build(:user, password: "123456", password_confirmation: "123456")
       user.valid?
       expect(user).to be_valid
     end
 
-    it "passwordが5文字以下であれば登録できない " do
-      user = build(:user, password: "00000", password_confirmation: "00000")
+    it "パスワードが5文字以下であれば登録できない " do
+      user = build(:user, password: "12345", password_confirmation: "12345")
       user.valid?
       expect(user.errors[:password]).to include("は6文字以上で入力してください")
     end
