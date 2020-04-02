f341f8457a874ab6ea923387f94bda0d2c6968f6
jun9130 <jun.nakama1030@gmail.com>
2020-04-02 22:52:49 +0900

    review.rbテスト記述

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :review do
-    
+    text    {"このお店はとても良い"}
+    user_id {"1"}
+    shop_id {"1"}
+    user
+    shop
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
+describe Review do
+  describe '#create' do
+    it "レビュー,user_id,shop_idがあれば登録ができる" do
+      review = build(:review)
+      expect(review).to be_valid
+    end
 
-RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "レビューがブランクだと登録ができない" do
+      review = build(:review, text: "")
+      review.valid?
+      expect(review.errors[:text]).to include("を入力してください")
+    end
+    
+    it "user_idが存在しないと登録ができない" do
+      review = build(:review, user_id: "")
+      review.valid?
+      expect(review.errors[:user]).to include("を入力してください")
+    end
+    
+    it "shop_idが存在しないと登録ができない" do
+      review = build(:review, shop_id: "")
+      review.valid?
+      expect(review.errors[:shop]).to include("を入力してください")
+    end
+  end
 end
