6f40f83704dc164f8d58b6d810141d02ed84e082
jun9130 <jun.nakama1030@gmail.com>
2020-05-12 00:30:01 +0900

    お気に入り機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :likes
   has_many :liked_shops, through: :likes, source: :shop
+  def already_liked?(shop)
+    self.likes.exists?(shop_id: shop.id)
+  end
 
   mount_uploader :user_image, ImageUploader
 
