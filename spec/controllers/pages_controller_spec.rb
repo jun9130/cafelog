8723a05470b66654e04f09777d5e19d31e4518fb
jun9130 <jun.nakama1030@gmail.com>
2020-11-27 16:59:50 +0900

    pages_controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do
+describe PagesController, type: :controller do
+  describe 'GET #privacy' do
+    it "privacy.html.hamlに遷移するか" do
+      get :privacy
+      expect(response).to render_template :privacy
+    end
+  end
 
+  describe 'GET #rule' do
+    it "rule.html.hamlに遷移するか" do
+      get :rule
+      expect(response).to render_template :rule
+    end
+  end
 end
